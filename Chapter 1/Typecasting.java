90a712a9d725fcecda2c7229b58db7851e2c06b9
Falak Patel <falakpatel213@gmail.com>
2025-01-26 15:15:03 +0530

    Update Typecasting

--- Chapter 1/Typecasting.java
+++ Chapter 1/Typecasting.java
@@ -1,12 +1,12 @@
 public class Typecasting {
     public static void main(String args[]) {
         int myInt = 9;
-        double myD = myInt; // Auto Conversion
+        double myD = myInt; // Auto Conversion(Widening)
         System.out.println(myInt);
         System.out.println(myD);
 
         double myD1 = 9.654;
-        int myInt1 = (int) myD1; // Auto Conversion
+        int myInt1 = (int) myD1; // Manual Conversion(Narrowing)
         System.out.println(myD1);
         System.out.println(myInt1);
     }
